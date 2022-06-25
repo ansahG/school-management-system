c67672827d074b14775627cbb360d1848c9842af
ansahG <ansahgideon590@gmail.com@>
2022-06-25 03:56:21 +0000

    changed the cases of some of the fle locations in controller as heroku cant find some views like the class view due to case sensitivity

--- app/Http/Controllers/Administrator/classController.php
+++ app/Http/Controllers/Administrator/classController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Administrator;
-use App\Models\classes\_Class;
+use App\Models\Classes\_Class;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
